526b460bd32d6486f566bb46ab33db6ab2c3aa7b
Gian Marco <gianmarco21sz@gmail.com>
2020-12-16 21:43:56 -0500

    cambiouno

--- src/app/user-pages/login/login.component.ts
+++ src/app/user-pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Empleado } from 'src/app/models/empleado';
 import { EmpleadoService } from 'src/app/services/empleado.service';
 declare var Swal : any;
+declare var asd : any;
 
 @Component({
   selector: 'app-login',
